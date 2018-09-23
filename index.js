2c3f5095866ee3d7547c030251333b6f3a89a4a4
Juan Nicolas <jnma91@gmail.com>
2018-09-23 18:08:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function sayHiToGrandma(string){
   if (whisper(string) === string){
     return "I can't hear you!"
   } else if (shout(string) === string) {
-    return "YES INDEED"
+    return "YES INDEED!"
   } else if (string === "I love you, Grandma.") {
     return "I love you, too."
   }
